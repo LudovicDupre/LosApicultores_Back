bae4b8d39392bcaa62991e7348347fee6afd6797
Ludovic Dupre <dupreludovic1@gmail.com>
2023-07-10 13:41:31 +0200

    first commit

---
+++ src/main/java/fr/fms/LosapicultoresBackContactApplication.java
@@ -0,0 +1,13 @@
+package fr.fms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LosapicultoresBackContactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LosapicultoresBackContactApplication.class, args);
+	}
+
+}

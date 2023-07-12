f33af1e5bba02eff5389d3caa75bb07e165c6321
Ayyoub MEHDIOUI <MehdiouiA@ad.fms-ea.com>
2023-07-12 10:52:41 +0200

    start contact

--- src/main/java/fr/fms/entities/Contact.java
+++ src/main/java/fr/fms/entities/Contact.java
@@ -1,4 +1,35 @@
 package fr.fms.entities;
 
-public class Contact {
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Contact implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private String address;
+    private String photo;
+
+
 }
